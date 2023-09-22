dc9f601cfd3e83018447bafe9abeedcc2150295f
SergioGomezAlvarez <sgmichielr@gmail.com>
2023-09-22 10:18:50 +0200

    Crud crud with raw queries, eloquent and query builder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +17,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //    fetch all users
+    //    $users = DB::select("select * from users");
+    //    $users = DB::table('users')->where('id', 1)->first();
+    $user = User::find(17);
+
+
+    //    create new user
+    //    $user = DB::insert('insert into users (name, email, password) values (?, ?, ?)', [
+    //    'Kenny',
+    //    'kenny31@bitfumess.com',
+    //    'password',
+    //    ]);
+    //    $user = DB::table('users')->insert([
+    //    'name' => 'Kenny',
+    //    'email' => 'kenny42@bbitfumes.com',
+    //    'password' => 'password'
+    //    ]);
+    //    $user = User::create([
+    //    'name' => 'Kenny',
+    //    'email' => 'kenny@bitfumestwee.com',
+    //    'password' => 'password',
+    //    ]);
+
+    //    update a user
+    //    $user = DB::update("update users set email=? where id=?", [
+    //    'plugz@plugz.com',
+    //    2,
+    //    ]);
+    //    $user = DB::table('users')->where('id', 3)->update(['email' => 'dyvex@bitfumes.com']);
+    //    $user = User::find(4);
+    //    $user->update([
+    //    'email' => 'kenny@bitfumes.com'
+    //    ]);
+
+
+    //    delete a user
+    //    $user = DB::delete("delete from users where id=2");
+    //    $user = DB::table('users')->where('id', 3)->delete();
+    //    $user = User::find(4);
+    //    $user->delete();
+    dd($user->name);
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +70,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
